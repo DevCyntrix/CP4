d672301e3d5bb4f41bac1fcc67c0f54f4a50fdbd
DevCyntrix <devcyntrix@hotmail.com>
2023-06-23 20:42:34 +0200

    Improve folia support

--- src/main/java/com/github/oasis/craftprotect/command/ClearItemCommand.java
+++ src/main/java/com/github/oasis/craftprotect/command/ClearItemCommand.java
@@ -2,17 +2,18 @@ package com.github.oasis.craftprotect.command;
 
 import com.github.oasis.craftprotect.api.CraftProtect;
 import com.github.oasis.craftprotect.api.CraftProtectCommand;
-import com.github.oasis.craftprotect.utils.M;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
 
 import static org.bukkit.Bukkit.getServer;
 
@@ -24,33 +25,32 @@ public class ClearItemCommand implements CraftProtectCommand {
     private CraftProtect plugin;
 
     @Override
+    public @Nullable String getPermission() {
+        return "cp4.admin";
+    }
+
+    @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
 
-        Player player = (Player) commandSender;
-
-        if (!player.hasPermission("cp4.admin")) {
-            plugin.sendMessage(player, M.NO_PLAYER);
-            return true;
-        }
-
-
         int countdownTime = 10;
-        new BukkitRunnable() {
+
+        Bukkit.getGlobalRegionScheduler().runAtFixedRate(plugin, new Consumer<>() {
             int countdown = countdownTime;
 
             @Override
-            public void run() {
+            public void accept(ScheduledTask scheduledTask) {
+
                 if (countdown == 0) {
                     clearItems();
-                    cancel();
+                    scheduledTask.cancel();
                     Bukkit.broadcastMessage(ChatColor.GREEN + "Alle Item-Drops wurden gelöscht!");
                 } else {
                     Bukkit.broadcastMessage(ChatColor.YELLOW + "Die Item-Drops werden in " + countdown + " Sekunden gelöscht.");
                     countdown--;
                 }
             }
-        }.runTaskTimer(plugin, 0, 20); // Führe die Aufgabe alle 20 Ticks (1 Sekunde) aus
-        return false;
+        }, 1, 20);// Führe die Aufgabe alle 20 Ticks (1 Sekunde) aus
+        return true;
     }
 
     public long clearItems() {
